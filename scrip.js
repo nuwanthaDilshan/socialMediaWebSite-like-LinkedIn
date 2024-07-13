74118af9e826a98d44f95fb179c5b4e3a7f5c9be
Nuwantha Dilshan <nuwanthadilshan56@gmail.com>
2024-07-13 16:56:40 +0530

    all web side media query completed

--- scrip.js
+++ scrip.js
@@ -1,5 +1,17 @@
 var profilemenu = document.querySelector(".profile-menu-wrap");
+var showMore = document.querySelector(".sidebar-activity");
+var moreLink =document.getElementById("showMoreLink")
 
 function profileMenuToggle() {
   profilemenu.classList.toggle("open-menu");
+}
+
+function sidebarActivityToggle() {
+  showMore.classList.toggle("open-activity");
+
+  if (showMore.classList.contains("open-activity")) {
+    moreLink.innerHTML = "Show less <b>-</b>"
+  } else {
+    moreLink.innerHTML = "Show more  <b>+</b>";
+  }
 }
